21a51bb898f1a6bdebc5cf4059def5ad375b5e1f
mardemlincow <mardem_lincow@yahoo.com.br>
2014-04-07 21:18:43 -0300

    Inicio da Aplicação com Rails 4

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dip3::Application.config.secret_key_base = 'b0377b5a6e7efe9a676ea0a013cecadd965c0d9f412d08769a8e3c49e251716257892bc0293620254affcbd177e23493e1afe483e8dd3146c8ba4495b5f7b025'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Dip3::Application.config.session_store :cookie_store, key: '_dip3_session'
